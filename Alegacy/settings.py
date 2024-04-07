979f0a0901c325f065057f1f60ec532262039b57
RaymondElisha <94136390+RaymondElisha@users.noreply.github.com>
2024-04-07 23:55:57 +0300

    Update settings.py

--- Alegacy/settings.py
+++ Alegacy/settings.py
@@ -88,6 +88,13 @@ WSGI_APPLICATION = 'Alegacy.wsgi.application'
     #}
 # }
 DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
+}
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
