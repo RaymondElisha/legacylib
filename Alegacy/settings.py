610742a6325682de13a8a63232eda9fdd5b542b8
AllanFdm <114208656+AllanFdm@users.noreply.github.com>
2024-04-09 10:17:55 +0300

    Update settings.py

--- Alegacy/settings.py
+++ Alegacy/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'Alegacy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
- DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
